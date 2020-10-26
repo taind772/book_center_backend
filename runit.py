4cf96a8e06eb2842656dc7ca63835658a3160890
zekken772 <ndtai772@gmail.com>
2020-10-26 13:14:45 +0700

    fix port

--- runit.py
+++ runit.py
@@ -9,7 +9,7 @@ TMP = ['./app/**/__pycache__', './app/**/migrations', './app/innerDB', './mysql/
 def clean():
   for path in TMP:
     for _ in glob.glob(path, recursive=True):
-      os.remove(_) if os.path.isfile(_) else shutil.rmtree(_)
+      os.remove(_) if os.path.isfile(_) else shutil.rmtree(_, ignore_errors=True)
 
 def run():
   # Clean docker data - dont uncomment this line if u dont know what it for
